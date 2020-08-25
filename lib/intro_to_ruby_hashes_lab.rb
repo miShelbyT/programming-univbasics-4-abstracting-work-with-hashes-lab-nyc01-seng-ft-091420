f792a0d1e612e8dc2a533e31cd9408ae3d3855c1
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-25 18:00:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  count = 0
-  if hash[key] = false
-    hash[key] = 1
-  else hash[key] += 1
+  if hash[key]
+    hash[key] += 1
+  else hash[key] = 1
   end
+  hash[key]
 end
